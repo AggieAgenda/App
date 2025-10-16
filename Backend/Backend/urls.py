1c5ec706b338fdad408900ee6676654608748ef7
DunsinK <133840905+DunsinK@users.noreply.github.com>
2025-10-16 00:01:43 -0500

    Setup notes on how to setup backend APIs in the urls.py

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("Calendar/", include("Calendar.urls")),
     path('admin/', admin.site.urls),
+    
 ]
--- Backend/Calendar/urls.py
+++ Backend/Calendar/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import google_login
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('auth/google/', google_login, name='google-login')
 ]
+# backend should expose endpoint http://localhost:8000/api/auth/google/
+# use axios.post(url)
+# have to install django-cors-headers because browser will block the frontend trying to hit backend since on two different ports
