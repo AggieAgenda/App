f3f49249ed599b83c96efaf21e550a1f615c5418
DunsinK <133840905+DunsinK@users.noreply.github.com>
2025-10-15 23:49:43 -0500

    more backend view stuff

--- Backend/Calendar/views.py
+++ Backend/Calendar/views.py
@@ -5,7 +5,7 @@ def index(request):
     return HttpResponse("Hello World. You're at the polls index.")
 
 # Create your views here.
-@api_view(['POST'])
+@api_view(['POST']) # not sure if this works yet.
 def google_login(request):
     token = request.data.get('token')
     idinfo = id_token.verify_oauth2_token(token, requests.Request())
