4c6b9688ff95ab9b9e27490a3f24edc16c83b7a9
DunsinK <133840905+DunsinK@users.noreply.github.com>
2025-10-15 23:44:37 -0500

    Started on Google Oauth through adding a view in the Calendar App. Likely will make new app just for API's

--- Backend/Calendar/views.py
+++ Backend/Calendar/views.py
@@ -5,3 +5,9 @@ def index(request):
     return HttpResponse("Hello World. You're at the polls index.")
 
 # Create your views here.
+@api_view(['POST'])
+def google_login(request):
+    token = request.data.get('token')
+    idinfo = id_token.verify_oauth2_token(token, requests.Request())
+    # logic to get or create user
+    return Response({"message": "Login successful"})
