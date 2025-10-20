a2a9a5aafe98bdaa9a04e644a4bd9c01417933b8
Deep10sun <Deep10sun@gmail.com>
2025-10-20 16:31:32 -0500

    initial setupt of calendar page

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center w-full px-10 py-6 bg-[#305d6f] text-white shadow-md">
-        <a href="/" className="text-3xl font-bold tracking-wide hover:text-[#800000] transition">
+    <nav className="flex justify-between items-center w-full px-10 py-6 bg-[#A83232] text-white shadow-md">
+        <a href="/" className="text-3xl font-bold tracking-wide hover:text-[#4B0000] transition">
           Aggie Agenda
         </a>
         <div className="space-x-8 text-lg">
-          <a href="/features" className="hover:text-[#800000] transition">Features</a>
-          <a href="/contact" className="hover:text-[#800000] transition">Contact</a>
-          <a href="/about" className="hover:text-[#800000] transition">About</a>
+          <a href="/features" className="font-bold hover:text-[#4B0000] transition">Features</a>
+          <a href="/contact" className="font-bold hover:text-[#4B0000] transition">Contact</a>
+          <a href="/about" className="font-bold hover:text-[#4B0000] transition">About</a>
         </div>
       </nav>
   );
