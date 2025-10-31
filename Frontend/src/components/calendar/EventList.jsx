d84af04650cceb67887e3763a11915871dd68bbd
Deep10sun <Deep10sun@gmail.com>
2025-10-30 19:06:17 -0500

    added missing/changed files

---
+++ Frontend/src/components/calendar/EventList.jsx
@@ -0,0 +1,10 @@
+export default function EventList(){
+    return (
+        <>
+            <div className="pt-8 pl-6 pr-6">
+                <h1 className="font-semibold text-gray-700 text-2xl">Upcoming</h1>
+                <hr className="border-t-4 border-gray-200 mx-auto my-4 rounded-3xl" />
+            </div>
+        </>
+    )
+}
