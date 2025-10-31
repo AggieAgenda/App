7e0f9990690ec897232ffa400cef08cc0c419b76
Deep10sun <Deep10sun@gmail.com>
2025-10-30 19:05:05 -0500

    added events display card to the right of calendar card

--- Frontend/src/pages/Calendar.jsx
+++ Frontend/src/pages/Calendar.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/Navbar.jsx"
 import CalendarNavBar from "../components/calendar/CalendarNavBar.jsx";
 import CalendarGrid from "../components/calendar/CalendarGrid.jsx";
+import EventList from "../components/calendar/EventList.jsx";
 import { useState } from 'react';
 
 export default function Calendar() {
@@ -13,11 +14,16 @@ export default function Calendar() {
   return (
     <>
       <NavBar />
-      <main className="bg-gray-100 p-8 overflow-y-auto h-[calc(100vh-4rem)] ">
+      <main className="bg-gray-100 p-8 overflow-y-auto h-[calc(100vh-4rem)] flex gap-8">
         <div className="bg-white w-250 h-200 rounded-4xl shadow-lg p-4"> {/* card */}
           <CalendarNavBar months={monthNames} setMonth={setCurrentMonth} setYear={setCurrentYear} month={currentMonth} year={currentYear} />
           <CalendarGrid month={currentMonth} year={currentYear}/>
-        </div>        
+        </div>  
+        <div className="flex justify-end">
+          <div className="bg-white w-100 h-200 rounded-4xl shadow-lg p-4">
+            <EventList />
+          </div>
+        </div>
       </main>
     </>
   );
