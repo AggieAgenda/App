8d60c31fa0e99e10ee31338b29b710c900e5abd2
Deep10sun <Deep10sun@gmail.com>
2025-11-20 13:43:00 -0600

    accepted incoming changes pt2

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,11 +4,15 @@
     import { BrowserRouter } from 'react-router-dom';
     import App from './App.jsx';
     import './index.css';
+    import { GoogleOAuthProvider } from '@react-oauth/google';
 
     ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
         <BrowserRouter>
-          <App />
+          <GoogleOAuthProvider clientId= 'process.env.REACT_APP_GOOGLE_CLIENT_ID' >
+            <App />
+          </GoogleOAuthProvider>
+          
         </BrowserRouter>
       </React.StrictMode>,
     );
