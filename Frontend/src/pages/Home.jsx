5768a9164329e4a0038551cb35eeb5611c08e0a4
Deep10sun <Deep10sun@gmail.com>
2025-11-20 13:38:10 -0600

    accepted incoming changes pt 2

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -12,6 +12,14 @@ export default function Home() {
 
   return (
     <div className="relative min-h-screen flex flex-col overflow-hidden bg-gradient-to-b from-[#f4d8aa] to-white text-[#1a1a1a]">
+      
+      {/* --- Background Animation --- */}
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+        {/* Floating gradient blobs */}
+        <div className="absolute w-72 h-72 bg-[#305d6f] rounded-full filter blur-3xl opacity-30 animate-float1 top-10 left-20"></div>
+        <div className="absolute w-96 h-96 bg-[#f4d8aa] rounded-full filter blur-3xl opacity-40 animate-float2 bottom-20 right-10"></div>
+        <div className="absolute w-64 h-64 bg-[#3c7289] rounded-full filter blur-3xl opacity-25 animate-float3 top-1/3 right-1/3"></div>
+      </div>
 
       {/* --- Navbar --- */}
       <NavBar /> 
