32c962d9d928ac93b90a840c375edfab44a462da
DunsinK <133840905+DunsinK@users.noreply.github.com>
2025-12-24 16:09:10 -0600

    comment protected route

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,11 +8,6 @@
 
      ReactDOM.createRoot(document.getElementById('root')).render(
       <React.StrictMode>
-        
-          
-            <App />
-          
-          
-        
+        <App /> 
       </React.StrictMode>,
     );
