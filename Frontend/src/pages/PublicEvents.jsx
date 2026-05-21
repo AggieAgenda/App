f2ec38ee9d9a67e658a16c04ce6b7c8c3f154579
DunsinK <133840905+DunsinK@users.noreply.github.com>
2026-05-20 23:19:41 -0500

    update url to env

--- Frontend/src/pages/PublicEvents.jsx
+++ Frontend/src/pages/PublicEvents.jsx
@@ -19,7 +19,7 @@ export default function PublicEvents(){
         setLoading(true);
         setError(null);
         try {
-            const response = await fetch("http://127.0.0.1:8000/api/events/");
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/events/`);
             if (!response.ok) {
                 throw new Error(`API error: ${response.status}`);
             }
